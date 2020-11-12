4505adcd28e11a9bd4e4d4f11b2e7c7077e39f6b
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-11-12 16:11:44 +0800

    added more error messages

--- src/main/Error.java
+++ src/main/Error.java
@@ -1,3 +1,12 @@
 public class Error {
+
+    static String MYSQL_DRIVER_NOT_FOUND = "[ERROR]: Java MySQL DB Driver not found!!";
+
     static String INVALID_INPUT = "[ERROR] Invalid Input";
+    static String INVALID_NUMBER_OF_PASSENGER = "[ERROR] Invalid number of passengers.";
+    static String START_LOCATION_NOT_FOUND = "[ERROR] Start Location not found.";
+    static String DESTINATION_NOT_FOUND = "[ERROR] Destination not found.";
+    static String SAME_START_LOCATION_AND_DESTIONATION = "[ERROR] Destination and start location should be different";
+    static String INVALID_DATE_FORMAT = "[ERROR] Invalid Date Format.";
+    static String INVALID_START_AND_END_DATE = "[ERROR] End Date should be greater than Start Date";
 }
