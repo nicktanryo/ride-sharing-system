3902c1b9ef100bf4816a07a1a6e4b99c28fb550c
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-11-21 19:28:00 +0800

    add error types and classifications

--- src/main/Error.java
+++ src/main/Error.java
@@ -3,11 +3,14 @@ public class Error extends Object{
     // database
     static String MYSQL_DRIVER_NOT_FOUND = "[ERROR]: Java MySQL DB Driver not found!!";
     static String QUERY_FAILURE = "[ERROR] Unable to make query";
+    static String SQL_EXCEPTION = "[ERROR] SQL Exception";
+    static String SQL_TIMEOUT_EXCEPTION = "[ERROR] SQL Timeout Exception";
 
     // general input
     static String INVALID_INPUT = "[ERROR] Invalid Input";
 
     // passanger
+    static String PASSENGER_HAS_OPEN_REQUEST = "[ERROR] Passenger has an open request";
     static String PASSENGER_ID_NOT_FOUND = "[ERROR] Passenger id not found";
     static String INVALID_NUMBER_OF_PASSENGER = "[ERROR] Invalid number of passengers.";
 
@@ -21,5 +24,22 @@ public class Error extends Object{
     static String INVALID_START_AND_END_DATE = "[ERROR] End Date should be greater than Start Date";
 
     // vehicle
-    static String CAR_MODEL_NOT_FOUND = "[ERROR] Car mmodel not found";
+    static String CAR_MODEL_NOT_FOUND = "[ERROR] Car model not found";
+    static String DRIVER_NOT_FOUND = "[ERROR] No drivers found, please adjust the criteria";
+
+    // trip
+    static String TRIPS_NOT_FOUND = "[ERROR] No trips found";
+
+    // driver
+    static String DRIVER_ID_NOT_FOUND = "[ERROR] Driver id not found";
+    static String DRIVER_LOCATION_FORMAT_INVALID = "[ERROR] Driver location format invalid";
+    static String NO_REQUEST_FOUND = "[ERROR] No request found";
+    static String LOCATION_FORMAT = "Location format: \"location_x location_y\"";
+    static String UNFINISHED_TRIP_NOT_FOUND = "[ERROR] Unfinished trip not found";
+    static String DRIVER_HAS_UNFINSHED_TRIP = "[ERROR] Driver has unfinished trip, Please complete the unfinished trip first";
+
+    // request
+    static String REQUEST_ID_NOT_FOUND = "[ERROR] Request Id not found";
+    static String REQUIREMENT_NOT_MATCH = "[ERROR] Driver's attirbutes do not match request requirements";
+    static String REQUEST_HAS_BEEN_TAKEN = "[ERROR] Request has been taken";
 }
